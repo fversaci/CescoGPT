b1bcbc72748c7d233b0d5c4f58c9e5268f57ac05
Francesco Versaci <francesco.versaci@gmail.com>
2024-05-11 19:00:33 +0200

    set default chunk to 64

--- src/bin/translate-subs.rs
+++ src/bin/translate-subs.rs
@@ -44,7 +44,7 @@ struct Args {
     /// Language to translate to
     lang: Lang,
     /// Number of blocks per query
-    #[arg(long, default_value_t = 32)]
+    #[arg(long, default_value_t = 64)]
     chunk: usize,
     /// Number of parallel translators
     #[arg(long, default_value_t = 1)]
